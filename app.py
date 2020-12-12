c1325281b2c5be74266c6391a4cd485aea0b15cb
Natalia <natalia.avivnotario@gmail.com>
2020-12-12 04:57:26 -0300

    favoritos

--- app.py
+++ app.py
@@ -175,6 +175,32 @@ def rate_movie():
 def get_rates_avgs():
     return jsonify({"rates_avgs": Rate.movies_rates_avgs()})
 
+@app.route('/favorites/user/<int:id>', methods=["GET"])
+@jwt_required
+def get_favorites_by_user(id):
+    favorites = Favorites.get_favorites(id)
+    return jsonify(favorites)
+
+@app.route('/favorites', methods=["POST"])
+@jwt_required
+def add_favorites():
+    if not request.is_json:
+        return jsonify({"msg": "El body o contenido esta vacio"}), 400
+
+    favorites = Favorites()
+    favorites.user_id = request.json.get("user_id", None)
+    favorites.movie_id = request.json.get("movie_id", None)
+
+    db.session.add(favorites)
+    db.session.commit()
+
+    return jsonify({"msg": "Agregado a favoritos exitosamente"})
+
+@app.route('/favorites/<int:id>', methods=["DELETE"])
+@jwt_required
+def delete_favorite(id):
+    Favorites.delete_favorite(id)
+    return jsonify({"success": True})
 
 if __name__ == "__main__":
     manager.run()
